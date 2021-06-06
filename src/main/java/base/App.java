c5785c355e168521cd54f90d151b45b66a575401
kiwibrie <brianne0128@gmail.com>
2021-06-05 22:56:20 -0500

    working base

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -11,5 +11,29 @@ public class App {
 
     public static void main(String[] args) {
         App myApp = new App();
+
+        String correctPassword = "abc$123";
+
+        String passwordAttempt = myApp.prompt();
+
+        if(passwordAttempt.equals(correctPassword)){
+            myApp.printCorrect();
+        } else {
+            myApp.printIncorrect();
+        }
     }
+
+    public void printCorrect(){
+        System.out.println("Welcome!");
+    }
+
+    public void printIncorrect(){
+        System.out.println("I don't know you.");
+    }
+
+    public String prompt(){
+        System.out.print("What is the password? ");
+        return in.nextLine();
+    }
+
 }
